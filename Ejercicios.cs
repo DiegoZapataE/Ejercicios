8fdd689bb348ddf4057e90031060e6abfb3207e7
DiegoZapataE <diegoze9012v@gmail.com>
2023-05-08 14:51:54 -0400

    nuevo

--- Ejercicios.cs
+++ Ejercicios.cs
@@ -34,3 +34,58 @@ public static void Fibonacci(int n)
     }
 }
 
+public static void CalcularEcuacionCuadratica(double a, double b, double c)
+{
+    double discriminante = b * b - 4 * a * c;
+
+    if (discriminante < 0)
+    {
+        Console.WriteLine("La ecuación no tiene solución real.");
+    }
+    else if (discriminante == 0)
+    {
+        double x = -b / (2 * a);
+        Console.WriteLine($"La única solución es x = {x}");
+    }
+    else
+    {
+        double x1 = (-b + Math.Sqrt(discriminante)) / (2 * a);
+        double x2 = (-b - Math.Sqrt(discriminante)) / (2 * a);
+        Console.WriteLine($"Las soluciones son x1 = {x1} y x2 = {x2}");
+    }
+}
+
+public static void CalcularIMC(double peso, double estatura)
+{
+    double imc = peso / (estatura * estatura);
+    Console.WriteLine("Su IMC es: " + imc.ToString("F"));
+
+    if (imc < 18)
+    {
+        Console.WriteLine("Peso bajo");
+    }
+    else if (imc < 25)
+    {
+        Console.WriteLine("Peso normal");
+    }
+    else if (imc < 27)
+    {
+        Console.WriteLine("Sobrepeso");
+    }
+    else if (imc < 30)
+    {
+        Console.WriteLine("Obesidad");
+    }
+    else if (imc < 35)
+    {
+        Console.WriteLine("Obesidad de tipo I");
+    }
+    else if (imc > 40)
+    {
+        Console.WriteLine("Obesidad de tipo II");
+    }
+    else
+    {
+        Console.WriteLine("Obesidad mórbida o extrema");
+    }
+}
